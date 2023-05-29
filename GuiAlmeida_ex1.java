e0b9ddcdf0b2c89b785b94026b3a00c22c8e84d1
Guilherme Almeida <131814344+GuiAlmeida141021@users.noreply.github.com>
2023-05-29 10:54:56 -0300

    Add files via upload

---
+++ GuiAlmeida_ex1.java
@@ -0,0 +1,19 @@
+//Guilherme 26/05/2023
+//Algoritmo que mostra todos os numeros inteiros de 1 a 1000
+
+//Import do Scanner para receber a resposta do usuario
+import java.util.Scanner;
+
+public class GuiAlmeida_ex1{
+    public static void main(String[] args){
+     Scanner leitor = new Scanner(System.in);
+        
+        // começo do for 
+        for(int i = 0; i < 1001; i++){
+            System.out.println( i);
+        }
+        
+        
+    }
+}
+//Fim
